e4af6ee8b4a4de5876768683ea007d5d9f7560dc
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2023-07-04 12:20:09 -0300

    modelo::all

---
+++ app/control/banco/CollectionShortcuts.php
@@ -0,0 +1,22 @@
+<?php
+
+use Adianti\Control\TPage;
+use Adianti\Database\TTransaction;
+use Adianti\Widget\Dialog\TMessage;
+
+class CollectionShortcuts extends TPage {
+    public function __construct() {
+        parent::__construct();
+        try {
+            TTransaction::open('curso');
+            $clientes = Cliente::all();
+            echo "<pre>";
+            var_dump($clientes);
+            echo "</pre>";
+            TTransaction::close();
+        } catch (Exception $e) {
+            new TMessage('error', $e->getMessage());
+        }
+        // parent::add();
+    }
+}
